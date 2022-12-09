65b02d68e2b6eb03a1aab6ecca83fab0a794001c
Sergey Lo <srglosev@gmail.com>
2022-12-09 13:47:15 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+
+st.title('Lit Finance Dashboard')
+
+tickers = ('TSLA', 'AAPL', 'MSFT', 'BTC-USD', 'ETH-USD')
+
+dropdown = st.multiselect('Pick your assets', tickers)
+
+start = st.date_input('Start', value=pd.to_datetime('2021-01-01'))
+end = st.date_input('Start', value=pd.to_datetime('today'))
+
+
+def relativeret(df):
+    rel = df.pct_change()
+    cumret = (1 + rel).cumprod() - 1
+    cumret = cumret.fillna(0)
+    return cumret
+
+
+if len(dropdown) > 0:
+    df = relativeret(yf.download(dropdown, start, end)['Adj Close'])
+    st.header(f'Return of {dropdown}')
+    st.line_chart(df)
